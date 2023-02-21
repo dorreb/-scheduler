6036f3f35ae26107343a08b9492ba1a0d0c39427
dorreb <ashley_ure@hotmail.com>
2023-02-21 14:25:14 -0500

    test: creating tests and fixing code to pass all scenarios

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -13,10 +13,22 @@ const DayListItem = (props) => {
     "day-list__item--full": props.spots === 0,
   });
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return 'no spots remaining';
+    }
+    if (spots === 1) {
+      return `1 spot remaining`;
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+
+  }
+
   return (
     <li className={dayClass} onClick={() =>  props.setDay(props.name)}>
       <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{props.spots} spots remaining</h3>
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
     </li>
   )
 }
