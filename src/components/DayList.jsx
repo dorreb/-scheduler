20914095bbfd9cdf860c05e583bc5dc8999d8ee9
dorreb <ashley_ure@hotmail.com>
2023-02-22 14:59:56 -0500

    refactor: update to controlled list naming conventions

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import DayListItem from './DayListItem';
+import React from "react";
+import DayListItem from "./DayListItem";
 
 // props days:Array an array of objects (each object represents a day and includes an id, name, and spots)
-// props day:String the currently selected day
-// props setDay:Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday"
+// props value:String the currently selected day
+// props sonChange:Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday"
 
 const DayList = (props) => {
   const dayListItems = props.days.map((day) => {
     return (
       <DayListItem
-      key ={day.id}
-      name={day.name}
-      spots={day.spots}
-      selected={day.name === props.day}
-      setDay={props.setDay}
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />
-    )
-  })
+    );
+  });
 
-  return (
-    <ul>
-      {dayListItems}
-    </ul>
-  )
-}
+  return <ul>{dayListItems}</ul>;
+};
 
-export default DayList
+export default DayList;
