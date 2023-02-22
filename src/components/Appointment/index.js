328e812d646cf70dcb6f85fb28bbaed22510a13d
dorreb <ashley_ure@hotmail.com>
2023-02-22 15:57:21 -0500

    feat: setting up appointment components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+// props time: '12pm' string
+
+const Appointment = () => {
+  return (
+    <article className="appointment"></article>
+  );
+};
+
+export default Appointment;
