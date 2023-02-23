e7e444e964c4b9f9a3c3c2240d08830145fb4e82
dorreb <ashley_ure@hotmail.com>
2023-02-23 15:06:10 -0500

    refactor: combine state for day and days

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,8 +52,15 @@ const appointments = {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: []
+    // appointments:{}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prevState => ({ ...prevState, days }));
+
 
   useEffect(() => {
     axios.get('/api/days')
@@ -86,8 +93,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
@@ -103,17 +110,3 @@ export default function Application(props) {
     </main>
   );
 }
-
-
-
-// should I have that function?
- // useEffect(() => {
-  //   const fetchDays = () => {
-  //     axios.get('/api/days')
-  //       .then((res) => {
-  //         setDays(res.data);
-  //       })
-  //       .catch((err) => console.log(err));
-  //   };
-  //   fetchDays();
-  // }, []);
