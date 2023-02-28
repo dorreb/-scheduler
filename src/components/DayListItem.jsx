e8cb9d1e262ddf62763816d4c27b59e804e1e8ae
dorreb <ashley_ure@hotmail.com>
2023-02-28 17:26:10 -0500

    test: mocking axios put request and implementing tests for the Application component

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import classNames from 'classnames'
+import React from "react";
+import classNames from "classnames";
 import "components/DayListItem.scss";
 
 // props name:String the name of the day
@@ -9,29 +9,31 @@ import "components/DayListItem.scss";
 
 const DayListItem = (props) => {
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected, 
+    "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   });
 
   const formatSpots = (spots) => {
     if (spots === 0) {
-      return 'no spots remaining';
+      return "no spots remaining";
     }
     if (spots === 1) {
       return `1 spot remaining`;
     } else {
       return `${props.spots} spots remaining`;
     }
-
-  }
+  };
 
   return (
-    <li className={dayClass} onClick={() =>  props.setDay(props.name)}>
-      <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
-  )
-}
+  );
+};
 
-export default DayListItem
-
+export default DayListItem;
